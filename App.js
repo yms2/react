1c8601fdb34a6d2825c5b1f44a3cce5d23465981
yms2 <90673332+yms2@users.noreply.github.com>
2022-06-27 18:06:33 +0900

    Add files via upload

---
+++ App.js
@@ -0,0 +1,44 @@
+import logo from './logo.svg';
+import './App.css';
+function Header(props){
+  console.log('props',props,props.title)
+  return  <header>
+  <h1><a href='/'>{props.title}</a></h1>
+</header>
+}
+function Nav(props){
+  const lis =[]
+    for(let i = 0; i <props.topics.length; i++){
+      let t = props.topics[i];
+      lis.push(<li key={t.id}><a href={'/read/ +t.id'}>{t.title}</a></li>)
+  
+  }
+  return  <nav>
+  <ol>
+    {lis}
+  </ol>
+</nav>
+}
+function Article(props){
+  return      <article>
+  <h2>{props.title}</h2>
+  {props.body}
+</article>
+}
+
+function App() {
+  const topics = [
+    {id:1,title:'html', body:'html is ...'},
+    {id:2,title:'cs', body:'html is ...'},
+    {id:3,title:'js', body:'html is ...'}
+  ]
+  return (
+    <div>
+      <Header title="REACT"></Header>
+      <Nav topics = {topics}></Nav>
+      <Article title="welcome"body="Hello, WEB"></Article>
+    </div>
+  );
+}
+
+export default App;
